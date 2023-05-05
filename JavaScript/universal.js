998b12a37d6fe16559bfeb6edeaccdf88490fa57
McEw3965 <EWAN.MCCARNEY22@bathspa.ac.uk>
2023-05-05 13:19:43 +0100

    Adds functionality to for pause button
    When clicked, the button will pause or play the animation based on its current state

--- JavaScript/universal.js
+++ JavaScript/universal.js
@@ -1,6 +1,8 @@
+//Donation section 
+
 let fiveBtn = document.getElementById("£5Button");
 let tenBtn = document.getElementById("£10Button");
-let twoFiveBtn = document.getElementById("£25Button");
+let twoFiveBtn = document.getElementById("£25Button"); 
 let donationDisplay = document.getElementById("donationDisplay");
 
 function donateFive() {
@@ -14,3 +16,24 @@ function donateTen() {
 function donateTwoFive() {
     document.getElementById("donationDisplay").innerHTML = "£25";
 }
+
+//Image Slider
+
+let pauseBtn = document.getElementById("pause");
+
+let isPaused = 0;
+
+function animationPause() {
+    if (isPaused === 0) {
+    document.getElementById("slide1").style.animationPlayState = 'paused';
+    document.getElementById("slide2").style.animationPlayState = 'paused';
+    document.getElementById("slide3").style.animationPlayState = 'paused';
+    isPaused = 1;
+    } else if (isPaused === 1) {
+        document.getElementById("slide1").style.animationPlayState = 'running';
+        document.getElementById("slide2").style.animationPlayState = 'running';
+        document.getElementById("slide3").style.animationPlayState = 'running';
+        isPaused = 0;
+    } 
+} 
+
