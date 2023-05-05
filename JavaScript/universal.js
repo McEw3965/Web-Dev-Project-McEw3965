877f73c642f1d3c9df24a4e1fd6e6dad967895c9
McEw3965 <EWAN.MCCARNEY22@bathspa.ac.uk>
2023-05-05 12:32:02 +0100

    Commits pulled from github caused some issues

---
+++ JavaScript/universal.js
@@ -0,0 +1,16 @@
+let fiveBtn = document.getElementById("£5Button");
+let tenBtn = document.getElementById("£10Button");
+let twoFiveBtn = document.getElementById("£25Button");
+let donationDisplay = document.getElementById("donationDisplay");
+
+function donateFive() {
+    document.getElementById("donationDisplay").innerHTML = "£5";
+}
+
+function donateTen() {
+    document.getElementById("donationDisplay").innerHTML = "£10";
+}
+
+function donateTwoFive() {
+    document.getElementById("donationDisplay").innerHTML = "£25";
+}
